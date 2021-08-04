f55a0019e6a6ad91ef90997563e12c14afe7b789
dominthebox <dsallustro@gmail.com>
2021-08-03 20:04:10 -0400

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,20 +1,19 @@
 // implement your posts router here
 const express =  require('express');
-const Posts = require('./posts-model');
+const Post = require('./posts-model');
 
 const router = express.Router()
 
 router.get('/', async (req, res) =>{
-    res.json('HAL HERE')
-    // try {
-    //     console.log('getting posts with async/await!')
-    //     const posts = await Posts.find()
-    //     res.status(200).json(posts)
-    // } catch (err) {
-    //     res.status(500).json({
-    //         message: "The posts information could not be retrieved",
-    //     })
-    // }
+    try {
+        console.log('getting posts with async/await!')
+        const posts = await Post.find()
+        res.status(200).json(posts)
+    } catch (err) {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+        })
+    }
 })
 
 router.get('/:id', (req, res) => {
